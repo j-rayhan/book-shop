677111fff6929ba83f39c29a7a768523685ff043
Jahirul Rayhan <johir.rayhan01@gmail.com>
2018-02-25 09:14:04 +0000

    CRUD OPERATIONS WITH REDUX

--- src/app.js
+++ src/app.js
@@ -9,6 +9,40 @@ const reducer = function(state={books:[]}, action){
 		 //  return {books};
 		 return {books: [...state.books, ...action.payload]};
 		 break;
+		 
+		 case "DELETE_BOOK":
+	    //CREATE A COPY OF THE CURRENT ARRAY OF BOOKS
+	     const currentBooks = [...state.books];
+	     //DETERMINE AT WHICH INDEX IN BOOKS ARRAY IS THE BOOK TO BE DELETED
+	     const indexToDelete = currentBooks.findIndex(
+	 	    	function(book){
+	 	    		return book.id === action.payload.id;
+	 	    	}
+	     	)
+	     // USE SLICE TO REMOVE THE BOOK AT THE SPECIFIED INDEX
+	     return {books: [...currentBooks.slice(0, indexToDelete), ...currentBooks.slice(indexToDelete + 1)]};
+	     break;
+
+	     case "UPDATE_BOOK":
+	     //CREATE A COPY OF THE CURRENT ARRAY OF BOOKS
+	     const currentBooksToUpdate = [...state.books];
+	     //DETERMINE AT WHICH INDEX IN BOOKS ARRAY IS THE BOOK TO BE DELETED
+	     const indexToUpdate = currentBooksToUpdate.findIndex(
+	 	    	function(book){
+	 	    		return book.id === action.payload.id;
+	 	    	}
+	     	)
+	     // CREATE A NEW BOOK BOJECT WITH THE NEW VALUES AND WITH THE SAME ARRAY INDEX OF THE ITEM WE WANT TO
+	     // REPLACE. TO ACHIEVE THIS WE WILL USE ...SPREAD BUT WE COULD USE CONCAT METHOS TO
+	     const newBookToUpdate = {
+	     	...currentBooksToUpdate[indexToUpdate],
+	    	title: action.payload.title
+	     }
+	     console.log('what is it newBookToUpdate ', newBookToUpdate);
+	     // USE SLICE TO REMOVE THE BOOK AT THE SPECIFIED INDEX, REPLACE WITH THE NEW OBJECT ANC CONCATE WITH 
+	     // THE REST OF ITEMS IN THE ARRAY
+	     return {books: [...currentBooksToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBooksToUpdate.slice(indexToUpdate + 1)]};
+	     break;
 		 }
 	 return state
 }
@@ -47,3 +81,15 @@ store.dispatch({
   price: 40
   }]
 })
+
+//delete action  
+ store.dispatch({
+ 	type:"DELETE_BOOK",
+ 	payload:{id:1}
+ })
+ 
+ //update action  
+ store.dispatch({
+ 	type:"UPDATE_BOOK",
+ 	payload:{id:2,title: 'New update title'}
+ })
