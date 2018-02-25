ec2c4c43ace4988a210c9fe138dcb872f2b093ea
Jahirul Rayhan <johir.rayhan01@gmail.com>
2018-02-25 04:14:26 +0000

    redux add action

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 import {createStore} from 'redux';
 
 //STEP 3 define reducers
-const reducer = function(state={}, action){
+const reducer = function(state=[], action){
 	 switch(action.type){
 		 case "POST_BOOK":
-		  return state = action.payload;
+		 let books = state.concat(action.payload);
+		  return books;
 		 break;
 		 }
 	 return state
@@ -21,10 +22,27 @@ store.subscribe(function(){
 // STEP 2 create and dispatch actions
 store.dispatch({
 	 type:"POST_BOOK",
-	 payload: {
-	 	id: 1,
-	 	title:'this is the book title',
-	 	description: 'this is the book description',
-	 	price: 33.33
-	 }
+	 payload: [{
+	 	 	id: 1,
+	 	 	title:'this is the book title',
+	 	 	description: 'this is the book description',
+	 	 	price: 33.33
+	 	 },{
+	 	 	id: 2,
+	 	 	title:'this is the Second book title',
+	 	 	description: 'this is the book description',
+	 	 	price: 36
+	 	 }]
+})
+
+
+// DISPATCH an other book
+store.dispatch({
+ type:"POST_BOOK",
+ payload: {
+ id: 3,
+ title:'this is the third book title',
+ description: 'this is the third book description',
+ price: 40
+ }
 })
